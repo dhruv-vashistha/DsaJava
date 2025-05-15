5ee0ba451b01057ccecbe3732d4053b12bba61e6
Dhruv Vashistha <dvas7337@gmail.com>
2025-05-15 15:02:52 +0530

    new file

--- src/com/company/dv_1_ArraysAndArraylist.java
+++ src/com/company/dv_1_ArraysAndArraylist.java
@@ -1,8 +1,27 @@
 package com.company;
 
 public class dv_1_ArraysAndArraylist {
+    static void putValue(int a){
+        int k = 0;
+        int[] rollNo;
+        rollNo = new int[100];
+        rollNo [k] = a;
+        k++;
+        for (int i= 0; i< k ; i++){
+            System.out.print(rollNo[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("methods in java");
+        System.out.println("Array in java");
+
+        putValue(3);
+        System.out.println();
+        putValue(3);
+        System.out.println();
+        putValue(99);
+        System.out.println();
+        putValue(38);
 
 
     }
