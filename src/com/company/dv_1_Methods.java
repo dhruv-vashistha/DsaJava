6a2719b87058f02582e874aaecfe8ea1cd11a663
Dhruv Vashistha <dvas7337@gmail.com>
2025-05-15 13:23:19 +0530

    some changes

---
+++ src/com/company/dv_1_Methods.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class dv_1_Methods {
+    public static void main(String[] args) {
+        System.out.println("methods in java");
+    }
+}
--- src/com/company/main.java
+++ src/com/company/main.java
@@ -1,7 +1,7 @@
 package com.company;
 
-public class Methods {
+public class main {
     public static void main(String[] args) {
-        System.out.println("methods in java");
+        System.out.println("nothing");
     }
 }
