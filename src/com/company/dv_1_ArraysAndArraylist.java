e096f351c6482c8e1691246aeb753f08b53ba2cd
Dhruv Vashistha <dvas7337@gmail.com>
2025-05-15 14:44:42 +0530

    changed name

--- src/com/company/dv_1_ArraysAndArraylist.java
+++ src/com/company/dv_1_ArraysAndArraylist.java
@@ -1,7 +1,9 @@
 package com.company;
 
-public class dv_1_Methods {
+public class dv_1_ArraysAndArraylist {
     public static void main(String[] args) {
         System.out.println("methods in java");
+
+
     }
 }
