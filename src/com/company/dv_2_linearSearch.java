5307d3859a6db71a9c5dc19e34477bf2326b71d9
Dhruv Vashistha <dvas7337@gmail.com>
2025-05-15 18:20:05 +0530

    adding the last changes

--- src/com/company/dv_2_linearSearch.java
+++ src/com/company/dv_2_linearSearch.java
@@ -1,9 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class dv_2_linearSearch {
     public static void main(String[] args) {
         //new topic now
-        System.out.println(44);
+        //question 1 - search for the number 14 in array
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter how many numbers you wanna put into the array");
+        int k = sc.nextByte();
+        int[] arr = new int[k];
+
+        //put all the values to the array
+        for(int i = 0; i<k; i++){
+            System.out.println("enter the " + i + "th index");
+            arr[i] = sc.nextInt();
+        }
+
+        //displaying the array
+        for (int element: arr) {
+            System.out.print(element + ", ");
+        }
+
 
     }
 }
